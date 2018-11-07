aaaddc9ea74a2a3dd300293c4bc9e35ef42a6e1d
Stousn <stefan.reip@edu.fh-joanneum.at>
2018-11-07 19:43:16 +0100

    dev_first_ui: First UI added

---
+++ Wearher/Wearher/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Wearher
+//
+//  Created by Stefan Reip on 07.11.18.
+//  Copyright © 2018 Stefan Reip. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
